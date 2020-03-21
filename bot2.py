86043ec68d98f6f4d451532c11d522b6382e6cad
NikitaKalinin <quilpboy@inbox.ru>
2020-03-21 04:49:59 +0300

    add smth

--- bot2.py
+++ bot2.py
@@ -1,13 +1,22 @@
-import discord
+﻿import discord
 from discord.ext import commands
 import random
 
-TOKEN = 'NjgyOTcxMzgzNzg5MTI1NjUz.XnVGbg.DeWGOkFXdGPEEy8apCWAJllrxXA'
+TOKEN = 'NjgyOTcxMzgzNzg5MTI1NjUz.XnVuRw.UarejcVg6hwSdE-RgO9nDpAXCUs'
 bot = commands.Bot(command_prefix='*')
 
 
+@bot.command(pass_context=True)  # разрешаем передавать агрументы
+async def roll(ctx, *args):
+    boards = list(map(int, args))
+    await ctx.send(random.randint(boards[0], boards[1]))
+
+    
 @bot.command(pass_context=False)  # разрешаем передавать агрументы
-async def roll(ctx):
-    await ctx.send(random.randint(0, 100))
+async def flip(ctx):
+    flag = random.randint(0, 1)
+    await ctx.send('reshka' if flag else 'orel')
+
+    
 
 bot.run(TOKEN)
